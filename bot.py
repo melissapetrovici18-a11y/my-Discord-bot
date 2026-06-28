305830bbcde71ab7a357131f0e85aa4b47feae5d
melissapetrovici18-a11y <melissapetrovici18@gmail.com>
2026-06-29 01:33:52 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.default())
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+@bot.command(name='hello')
+async def hello(ctx):
+    await ctx.send(f'Hello {ctx.author.name}!')
+
+bot.run(os.getenv('TOKEN'))
