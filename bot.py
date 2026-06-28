3249a2f0131467cc624e5b4b721ec08534b51fa3
melissapetrovici18-a11y <melissapetrovici18@gmail.com>
2026-06-29 01:43:50 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import os
 import discord
 from discord.ext import commands
-bot = commands.Bot(command_prefix='/', intents=discord.Intents.default())
+
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.default())
 
 @bot.event
 async def on_ready():
